0e05f283c3e609e10495dfc0def494c694a8c64c
Andrew Thomas Chan <achan136@unix3.csc.calpoly.edu>
2024-11-20 23:20:43 -0800

    parse CL args & sig handler

--- server.c
+++ server.c
@@ -1,9 +1,49 @@
 #include "smartalloc.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <signal.h>
+
+int running = 1;
+
+void sigint_handler(int sig) {
+    running = 0;
+}
 
 int main(int argc, char **argv)
 {
-    printf("server running...\n");
+    signal(SIGINT, sigint_handler);
+    struct sockaddr_in v4;
+    struct sockaddr_in6 v6;
+
+    if (argc != 1 && argc != 2) {
+        printf("Usage: %s [IP address]\n", argv[0]);
+        return 1;
+    }
+
+    if (argc == 2) {
+        if (inet_pton(AF_INET, argv[1], &(v4.sin_addr)) == 1) {
+            printf("Valid IPv4 address: %s\n", argv[1]);
+        } else if (inet_pton(AF_INET6, argv[1], &(v6.sin6_addr)) == 1) {
+            printf("Valid IPv6 address: %s\n", argv[1]);
+        } else {
+            printf("Invalid IP address: %s\n", argv[1]);
+            return 1;
+        }
+    } else {
+        printf("No IP address provided, binding to all interfaces.\n");
+    }
+
+    while (running) {
+        printf("server running...\n");
+        sleep(5);
+    }
+
+    printf("\nServer exiting cleanly.\n");
+
+    // clean up stuff
+
     return 0;
 }
